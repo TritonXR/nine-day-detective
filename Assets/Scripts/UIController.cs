59f0cfe19cbc5217844d5a3e778ab78412db61e9
Davin Tjong <davintjong@gmail.com>
2019-01-22 15:16:52 -0800

    Added popup window

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour {
+    public RawImage popup; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            popup.gameObject.SetActive(!popup.gameObject.activeSelf);
+        }
+		
+	}
+}
