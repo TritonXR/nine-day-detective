7e39a5bbecb37745f031b1adbfb73f8973d131b0
Elton H Ho <eho@UCSD.EDU>
2019-01-27 18:30:52 -0800

    created some problems but updated the horizontal movement script

--- Assets/Scripts/Horizontal_Movement.cs
+++ Assets/Scripts/Horizontal_Movement.cs
@@ -4,22 +4,45 @@ using UnityEngine;
 
 public class Horizontal_Movement : MonoBehaviour {
 
-    float position = 0;
     public float xPosition;
     public float yPosition;
     public float zPosition;
-    Vector3 horizontalMovement; 
+    float speed = 0.01f;
+
+    public Camera camera;
+
+    //variables for casting the ray
+    float rayLength = 3.0f;
+
 	// Use this for initialization
 	void Start () {
+
+        camera = GetComponent<Camera>();
 		
     }
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Translate(-position, 0, 0);
-        if (position < 0.01f)
+
+        RaycastHit hit;
+        Ray cursorRay = camera.ScreenPointToRay(new Vector3(Screen.width / 2f,
+                                                 Screen.height / 2f, 0));
+        if (Physics.Raycast(cursorRay, out hit, rayLength))
         {
-            position += 0.001f;
+            if (hit.collider.tag == "Drawer3Handle")
+            {
+                OpenDrawer();
+            }
         }
+
 	}
+
+    private void OpenDrawer ()
+    {
+        if (xPosition > -5.0f)
+        {
+            transform.Translate(-speed, 0, 0);
+            xPosition -= speed;
+        }
+    }
 }
