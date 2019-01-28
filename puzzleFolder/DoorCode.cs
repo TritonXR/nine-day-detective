908e7b8a6dd4a850140e5708ab53e282df5ebdf7
Wen-ling Chen <wec064@UCSD.EDU>
2019-01-27 19:26:19 -0800

    Update Jan 27 Add PuzzleScript.cs DoorCode.cs and DoorButton.cs to master puzzleFolder

---
+++ puzzleFolder/DoorCode.cs
@@ -0,0 +1,89 @@
+﻿public class DoorCode
+{
+
+    //The code in string format
+    const string CORRECT_COMBINATION = "4190";
+
+    //The current combination
+    string currentCombination;
+
+    //Whether the door is open or not
+    bool isOpen;
+
+
+    /**
+	 * Constructor for the DoorCode object; initiates the combinatiion (string) and a boolean.
+	 *
+	 */
+    public DoorCode()
+    {
+        this.currentCombination = "";
+        this.isOpen = false;
+    }
+
+
+    /**
+	 * Adds a number to the current combination and checks for the correct code 
+	 *
+	 * @param number: the string that holds a number representing the button pressed
+	 */
+    public void ButtonPressed(string number)
+    {
+        this.currentCombination += number;
+
+        //Once all four numbers are inputted
+        if (this.currentCombination.Length == CORRECT_COMBINATION.Length)
+        {
+            if (CombinationCheck())
+            {
+                //door unlocks, victory popup	
+            }
+            else
+            {
+                ResetCode();
+                //wrong code popup
+            }
+        }
+    }
+
+
+    /**
+	 * Checks the combination once all four numbers are inputted.
+	 */
+    public bool CombinationCheck()
+    {
+        //Verifies the code 
+        if (string.Equals(CORRECT_COMBINATION, this.currentCombination))
+        {
+            return true;
+        }
+        return false;
+    }
+
+
+    /**
+	 * Resets the current combination
+	 *
+	 */
+    public void ResetCode()
+    {
+        this.currentCombination = "";
+    }
+
+    /**
+	 * Gets the current combination
+	 *
+	 */
+    public string getCurrentCode()
+    {
+        return this.currentCombination;
+    }
+
+
+
+    // array of numbers pressed
+
+    // display 
+
+    // code check
+}
---
+++ puzzleFolder/PuzzleScript.cs
@@ -0,0 +1,87 @@
+﻿
+public class PuzzleGame
+{
+    //Array of tiles
+    Tile[] tileArray;
+    //Array of ints to initiate Tiles in the wrong order
+    int[] tileOrder = { }; //choose the order of numbers 0-8 <---
+                           // 8th tile is the empty one
+
+
+    /**
+	 * Constructor for the PuzzleGame. Initiates tileArray and makes the Tiles in puzzle order.
+	 */
+    public PuzzleGame()
+    {
+        this.tileArray = new Tile[9];
+        for (int i = 0; i < (this.tileArray.Length - 1); i++)
+        {
+            this.tileArray[i] = new Tile(false, tileOrder[i]);
+        }
+        this.tileArray[8] = new Tile(true, tileOrder[8]);
+    }
+
+
+    /**
+	 * Checks whether the user has won. Call this method everytime a tile is moved.
+	 */
+    public bool CheckWin()
+    {
+        for (int i = 0; i < tileArray.Length; i++)
+        {
+            if (this.tileArray[i].tileNumber != i)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+	 * Switches the empty Tile and the pressed Tile.
+	 *
+	 * @param tileNumber: The Number of the Tile pressed.
+	 */
+    public void TilePressed(int tileNumber)
+    {
+        Tile[] newTileArray = new Tile[9];
+        for (int i = 0; i < this.tileArray.Length; i++)
+        {
+            //If it is the Tile pressed, make it empty
+            if (tileArray[i].tileNumber == tileNumber)
+            {
+                newTileArray[i] = new Tile(true, 8);
+            }
+            //If it is the empty Tile, make it the Tile pressed
+            else if (tileArray[i].tileNumber == 8)
+            {
+                newTileArray[i] = new Tile(false, tileNumber);
+            }
+            //If it isn't any of the above, copy the same tile over.
+            else
+            {
+                newTileArray[i] = new Tile(false, tileArray[i].tileNumber);
+            }
+        }
+        this.tileArray = newTileArray;
+    }
+
+
+}//End class PuzzleGame
+
+
+public class Tile
+{
+
+    public bool isEmpty;
+    public int tileNumber;
+
+    public Tile(bool isEmpty, int tileNumber)
+    {
+        this.isEmpty = isEmpty;
+        this.tileNumber = tileNumber;
+    }
+
+
+}
+
