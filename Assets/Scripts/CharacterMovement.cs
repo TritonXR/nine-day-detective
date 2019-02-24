50cfa3041df5ccc13758d875d20c5767924be591
Wen-ling Chen <wec064@UCSD.EDU>
2019-02-23 17:55:10 -0800

    FINALLY GET THE JOYSTICK WORKS

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using OVRSimpleJSON;
+using OVRTouchSample;
 
 public class CharacterMovement : MonoBehaviour
 {
@@ -14,10 +16,27 @@ public class CharacterMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        OVRInput.Update();
+        OVRInput.FixedUpdate();
+        //float translation = Input.GetAxis("Vertical") * speed;
+        //float straffe = Input.GetAxis("Horizontal") * speed;
+        //translation *= Time.deltaTime;
+        //straffe *= Time.deltaTime;
+
+        //transform.Translate(straffe, 0, translation);
+
+        if (OVRInput.GetDown(OVRInput.Button.One))
+        {
+            Debug.Log("PressedOne");
+        }
+
+        
+
+
         Vector2 translation = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick) * speed;
         translation *= Time.deltaTime;
 
-        transform.position += new Vector3(translation.x, translation.y, 0);
+        transform.position += new Vector3(translation.x, 0, translation.y);
 
         if (Input.GetKeyDown("escape"))
             Cursor.lockState = CursorLockMode.None;
