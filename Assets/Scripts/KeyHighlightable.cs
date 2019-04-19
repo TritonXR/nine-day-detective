642e707027c8c4e755b4395774ccd84e3aff3de8
Titan Ngo <ttngo@ucsd.edu>
2019-04-19 16:45:06 -0700

    Added backspace functionality and freezes all interactions with keypad once solved

--- Assets/Scripts/KeyHighlightable.cs
+++ Assets/Scripts/KeyHighlightable.cs
@@ -20,69 +20,87 @@ public class KeyHighlightable : MonoBehaviour
 		CodeSolved = false;
 	}
 
-	void Update() 
-	{
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Debug.Log("button pressed");
-            // Get buttons pressed
-            if (triggers[0].highlighted == true)
-            {
-                current += "1";
-            }
-            if (triggers[1].highlighted == true)
-            {
-                current += "2";
-            }
-            if (triggers[2].highlighted == true)
-            {
-                current += "3";
-            }
-            if (triggers[3].highlighted == true)
-            {
-                current += "4";
-            }
-            if (triggers[4].highlighted == true)
-            {
-                current += "5";
-            }
-            if (triggers[5].highlighted == true)
-            {
-                current += "6";
-            }
-            if (triggers[6].highlighted == true)
-            {
-                current += "7";
-            }
-            if (triggers[7].highlighted == true)
-            {
-                current += "8";
-            }
-            if (triggers[8].highlighted == true)
-            {
-                current += "9";
-            }
-            if (triggers[9].highlighted == true)
-            {
-                current += "0";
-            }
-        }
+    void Update()
+    {
 
-        // Compare current
-        if (current.Length == 4) {
-			if (current.Equals(CORRECT)) {
-				CodeSolved = true;
-			} else {
-				// Reset current
-				current = "";
-			}
-		}
-		
-		// For testing
-		Debug.Log("inputted code is: " + current);
-		if (CodeSolved) {
-			Debug.Log("CodeSolved!");
-		}
+        // Freeze interactions with the keypad if the code is solved
+        if (!CodeSolved)
+        {
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Debug.Log("button pressed");
+                // Get buttons pressed
+                if (triggers[0].highlighted == true)
+                {
+                    current += "1";
+                }
+                if (triggers[1].highlighted == true)
+                {
+                    current += "2";
+                }
+                if (triggers[2].highlighted == true)
+                {
+                    current += "3";
+                }
+                if (triggers[3].highlighted == true)
+                {
+                    current += "4";
+                }
+                if (triggers[4].highlighted == true)
+                {
+                    current += "5";
+                }
+                if (triggers[5].highlighted == true)
+                {
+                    current += "6";
+                }
+                if (triggers[6].highlighted == true)
+                {
+                    current += "7";
+                }
+                if (triggers[7].highlighted == true)
+                {
+                    current += "8";
+                }
+                if (triggers[8].highlighted == true)
+                {
+                    current += "9";
+                }
+                if (triggers[9].highlighted == true)
+                {
+                    current += "0";
+                }
+
+                // Delete button
+                if (triggers[10].highlighted == true)
+                {
+                    current = current.Remove(current.Length - 1);
+                }
+            }
+
+            // Compare current
+            if (current.Length == 4)
+            {
+                if (current.Equals(CORRECT))
+                {
+                    CodeSolved = true;
+                }
+                else
+                {
+                    // Reset current
+                    current = "";
+                }
+            }
+
+            // For testing
+            Debug.Log("inputted code is: " + current);
+            if (CodeSolved)
+            {
+                Debug.Log("CodeSolved!");
+            }
+
+        }
 	}
 
    
