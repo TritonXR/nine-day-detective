86460e37ea56c457a68bf7d01968854d6af806f4
Titan Ngo <ttngo@ucsd.edu>
2019-05-17 17:34:57 -0700

    Finished puzzle

--- Assets/Scripts/PuzzleHighlightable.cs
+++ Assets/Scripts/PuzzleHighlightable.cs
@@ -75,7 +75,7 @@ public class PuzzleHighlightable : MonoBehaviour
                     {
                         str += currentArray[j] + ",";
                     }
-                    Debug.Log("Array is " + str);
+                    //Debug.Log("Array is " + str);
                     
                 }
             }
