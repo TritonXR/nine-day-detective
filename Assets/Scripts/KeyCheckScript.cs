031b3c818f40588cff01f0060883d6426aefe824
Elton H Ho <eho@UCSD.EDU>
2019-05-31 17:43:41 -0700

    updated the scene with the key and box

---
+++ Assets/Scripts/KeyCheckScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyCheckScript : MonoBehaviour
+{
+    public Renderer rend;
+    public string colliderName;
+    private bool keyClicked = false;
+    float rayLength = 10.0f;
+    public Camera camera;
+
+    void Start()
+    {
+        Debug.Log("In KeyCheckScript");
+    }
+
+    // Toggle the Object's visibility each second.
+    void Update()
+    {
+        RaycastHit hit;
+        Ray cursorRay = camera.ScreenPointToRay(new Vector3(Screen.width / 2f, Screen.height / 2f, 0));
+
+        if (Physics.Raycast(cursorRay, out hit, rayLength))
+        {
+            if (hit.collider.gameObject.name == colliderName && hit.collider.isTrigger)
+            {
+                if (Input.GetKeyDown(Constants.interactionKey))
+                {
+                    Debug.Log("Box has been clicked");
+                    if (!rend.enabled)
+                    {
+                        Debug.Log("Box has been opened because you have the key");
+                    }
+                    else
+                    {
+                        Debug.Log("Box cannot be opened because you don't have the key yet");
+                    }
+                }
+
+            }
+        }
+        // Find out whether current second is odd or even
+
+
+        
+    }
+}
---
+++ Assets/Scripts/KeyRenderer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyRenderer : MonoBehaviour
+{
+    public Renderer rend;
+    public string keyColliderName;
+    private bool keyClicked = false;
+    float rayLength = 10.0f;
+    public Camera camera;
+
+    void Start()
+    {
+        rend.enabled = true;
+    }
+
+    // Toggle the Object's visibility each second.
+    void Update()
+    {
+        RaycastHit hit;
+        Ray cursorRay = camera.ScreenPointToRay(new Vector3(Screen.width / 2f, Screen.height / 2f, 0));
+
+        if (Physics.Raycast(cursorRay, out hit, rayLength))
+        {
+            if (hit.collider.gameObject.name == keyColliderName && hit.collider.isTrigger)
+            {
+                Debug.Log("you have hit the key");
+                if (Input.GetKeyDown(Constants.interactionKey))
+                {
+                    Debug.Log("You have obtained the key");
+                    keyClicked = true; 
+                }
+            }
+        }
+        // Find out whether current second is odd or even
+
+
+        // Enable renderer accordingly
+        rend.enabled = !keyClicked;
+    }
+}
